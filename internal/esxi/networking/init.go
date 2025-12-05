5ce1bf54cb7d1a299c2839048450b1a40a4f1b9c
ham_bn <ham.gh.b.n@gmail.com>
2025-12-05 15:32:48 +0330

    Reorganize networking directory: rename 'network' to 'adapters' for clarity
    Aligns with proper directory structure:
    - internal/esxi/networking/adapters/ (physical network adapters/vmnic)
    - internal/esxi/networking/portgroup/ (virtual port groups)
    - internal/esxi/networking/vswitch/ (virtual switches)
    - internal/esxi/networking/vmknics/ (VM kernel NICs)
    
    Commands remain unchanged:
    - list-network-adapters (lists physical network adapters)
    - list-portgroups, list-vswitches, list-vmknics (other networking resources)
    
    Updated package name from 'network' to 'adapters' in both files.
    Updated networking/init.go to import from correct package path.

--- internal/esxi/networking/init.go
+++ internal/esxi/networking/init.go
@@ -4,7 +4,7 @@ package networking
 // which triggers their init() functions for command registration
 
 import (
-	_ "github.com/esxi-manager/esxi-manager/internal/esxi/networking/network"
+	_ "github.com/esxi-manager/esxi-manager/internal/esxi/networking/adapters"
 	_ "github.com/esxi-manager/esxi-manager/internal/esxi/networking/portgroup"
 	_ "github.com/esxi-manager/esxi-manager/internal/esxi/networking/vmknics"
 	_ "github.com/esxi-manager/esxi-manager/internal/esxi/networking/vswitch"
